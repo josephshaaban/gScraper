69f8601a7397f15a439a9687b1e613022148e728
Youssef Shaaban <joseph.sha3ban@gmail.com>
2024-01-17 19:07:51 +0400

    test deplyement with chromium

--- index.mjs
+++ index.mjs
@@ -4,9 +4,10 @@ import chromium from "@sparticuz/chromium";
 // const chromium = require("@sparticuz/chromium");
 
 export const handler = async (event) => {
+  var version, title;
   try {
     const browser = await puppeteer.launch({
-      args: chromium.args,
+      args: [...chromium.args, "--no-sandbox"],
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(),
       headless: chromium.headless,
@@ -19,23 +20,24 @@ export const handler = async (event) => {
       "https://docs.aws.amazon.com/cli/latest/userguide/getting-started-prereqs.html",
       { waitUntil: "networkidle0" }
     );
-
-    console.log("Chromium:", await browser.version());
-    console.log("Page Title:", await page.title());
+    version = await browser.version();
+    title = await page.title();
+    console.log("Chromium:", version);
+    console.log("Page Title:", title);
 
     await page.close();
 
     await browser.close();
   } catch (error) {
-    throw new Error(error.message);
+    console.error(error);
   }
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: {
-          Chromium: await browser.version(),
-          PageTitle: await page.title(),
+          Chromium: version,
+          PageTitle: title,
         },
         input: event,
       },
