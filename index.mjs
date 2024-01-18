963023f02657186bee46fb16803cadfdf391f38d
Youssef Shaaban <joseph.sha3ban@gmail.com>
2024-01-19 03:53:10 +0400

    Anti-captcha by avoiding

--- index.mjs
+++ index.mjs
@@ -6,7 +6,9 @@ import { getRandomInt } from "./utils.mjs";
 
 export const handler = async (event) => {
   var gData, pageTitle;
+  const loadImages = false;
   try {
+    // Setup puppeteer browser
     const browser = await puppeteer.launch({
       args: [...chromium.args, "--no-sandbox"],
       defaultViewport: chromium.defaultViewport,
@@ -15,8 +17,7 @@ export const handler = async (event) => {
       ignoreHTTPSErrors: true,
     });
 
-    const page = await browser.newPage();
-
+    // Serialize final google URL which initialized with search query
     const searchQuery = "Web3";
     const params = {
       rpcids: "HoAMBc",
@@ -33,9 +34,88 @@ export const handler = async (event) => {
       .join("&");
     const url = `https://www.google.com/search?${query}`;
 
+    // Start new page
+    const page = await browser.newPage();
+
+    // Custom user agent
+    // TODO: randomize selecting user agent
+    const userAgent =
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/119.0.0.0 Safari/537.36";
+    await page.setUserAgent(userAgent);
+
+    // Custom viewport
+    await page.setViewport({
+      width: 1920 + Math.floor(Math.random() * 100),
+      height: 3000 + Math.floor(Math.random() * 100),
+      deviceScaleFactor: 1,
+      hasTouch: false,
+      isLandscape: false,
+      isMobile: false,
+    });
+
+    //
+    await page.setJavaScriptEnabled(true);
+    await page.setDefaultNavigationTimeout(0);
+
+    // Avoid loading un-necessary
+    if (!loadImages) {
+      await page.setRequestInterception(true);
+      page.on("request", (req) => {
+        if (
+          req.resourceType() == "stylesheet" ||
+          req.resourceType() == "font" ||
+          req.resourceType() == "image"
+        ) {
+          req.abort();
+        } else {
+          req.continue();
+        }
+      });
+    }
+
+    // Pass webdriver check
+    await page.evaluateOnNewDocument(() => {
+      Object.defineProperty(navigator, "webdriver", {
+        get: () => false,
+      });
+    });
+
+    // Pass chrome check
+    await page.evaluateOnNewDocument(() => {
+      window.chrome = {
+        runtime: {},
+      };
+    });
+
+    // Pass plugins check
+    await page.evaluateOnNewDocument(() => {
+      const originalQuery = window.navigator.permissions.query;
+      return (window.navigator.permissions.query = (parameters) =>
+        parameters.name === "notifications"
+          ? Promise.resolve({ state: Notification.permission })
+          : originalQuery(parameters));
+    });
+
+    // Overwrite the `plugins` property to use a custom getter.
+    await page.evaluateOnNewDocument(() => {
+      Object.defineProperty(navigator, "plugins", {
+        // This just needs to have `length > 0` for the current test,
+        // but we could mock the plugins too if necessary.
+        get: () => [1, 2, 3, 4, 5],
+      });
+    });
+
+    // Overwrite the `plugins` property to use a custom getter.
+    await page.evaluateOnNewDocument(() => {
+      Object.defineProperty(navigator, "languages", {
+        get: () => ["en-US", "en"],
+      });
+    });
+
     await page.goto(url, { waitUntil: "networkidle0" });
     pageTitle = await page.title();
 
+    // Crowling the google page and scrape search data
     var gData = await page.evaluate(() => {
       var data = [];
 
